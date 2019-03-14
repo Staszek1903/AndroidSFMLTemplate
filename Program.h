0aa2624be7ff4a93072fb80a025e76431c0a34b4
root <root@localhost>
2019-03-14 14:40:33 +0100

    program h

---
+++ Program.h
@@ -0,0 +1,25 @@
+#ifndef PROGRAM_H_
+#define PROGRAM_H_
+
+#include "Console.h"
+#include <SFML/Graphics.hpp>
+
+class Program 
+{
+	void input();
+	void render();
+protected:
+	sf::RenderWindow win;
+ 
+    virtual void onRender();
+	virtual void onUpdate();
+	virtual void onEvent(sf::Event &ev);
+	
+public:
+	Program();
+	~Program();
+	
+	void run();
+};
+
+#endif /* PROGRAM_H_ */
