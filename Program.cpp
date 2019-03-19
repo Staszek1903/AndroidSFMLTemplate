edfc2139f8be805973a1d7bb624f1001ba1456df
root <root@localhost>
2019-03-19 20:51:29 +0100

    program cpp

--- Program.cpp
+++ Program.cpp
@@ -15,11 +15,11 @@ Program::~Program()
 
 void Program::run()
 {
-    int tick = 0;
+    //int tick = 0;
 	while(win.isOpen())
 	{
-        Console::get()<<"tick: "<<tick<<'\n';
-        tick++;
+        //Console::get()<<"tick: "<<tick<<'\n';
+        //tick++;
 		input();
 		onUpdate();
 		render();
