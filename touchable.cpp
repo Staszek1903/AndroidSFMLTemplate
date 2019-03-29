f0720e651b0ed164bd5926e02441312fd8ff2f21
root <root@localhost>
2019-03-29 16:01:04 +0100

    touchable cpp

---
+++ touchable.cpp
@@ -0,0 +1,66 @@
+#include "touchable.h"
+
+Touchable::Touchable(float x, float y, float w, float h)
+	:handler(nullptr)
+{
+	setPosition(x,y);
+	create_rect(w,h);
+}
+
+void Touchable::handle(const TouchEvent & ev)
+{
+	float x,y;
+	Renderer::get().mapPtoC(ev.x,ev.y,x,y);
+	auto b = getGlobalBounds();
+		
+	if(x>b.left&&x<(b.left+b.width)&& y>b.top&&y<(b.top+b.height))
+	{
+	 	
+	} 
+}
+
+std::size_t Touchable::getPointCount() const
+{
+	return points.size();
+}
+
+sf::Vector2f Touchable::getPoint(std::size_t i) const
+{
+	return points.at(i);
+}
+
+void Touchable::create_rect(float w, float h)
+{
+	points = { {0.0f,0.0f},
+						{w, 0.0f},
+						{w,h},
+						{0.0f,h}};
+	update();
+}
+	
+void Touchable::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
+	const int size = points.size();
+	sf::VertexArray array(sf::TriangleFan, size);
+	
+	states.transform = getTransform();
+	states.texture = getTexture();
+	
+	for(int i=0; i<size; ++i)
+	{
+		array[i].position = points.at(i);
+		array[i].color = getFillColor();
+	}
+	
+	target.draw(array, states);
+}
+
+void Touchable::setHandler(TouchableHandler & h)
+{
+	handler = &h;
+}
+
+Touchable::~Touchable()
+{
+	if(handler) delete handler;
+}
