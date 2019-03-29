3f55681ac0c0a5211dbe0589f543c64cfc516ebc
root <root@localhost>
2019-03-29 16:32:16 +0100

    p cpp

--- p.cpp
+++ p.cpp
@@ -1,10 +1,16 @@
 #include "p.h"
 
 P::P()
+:t1(100,500,100,100), t2(300,500, 100,100)
 {
     s.setPosition(sf::Vector2f (200,200));
     s.setFillColor(sf::Color::Blue);
     s.setRadius(100);
+ 	t1.setFillColor(sf::Color::Blue);
+ 	t2.setFillColor(sf::Color::Green);   
+	
+	TouchBuffer::get().addHandler(t1);
+	TouchBuffer::get().addHandler(t2);
 }
 
 void P::onUpdate()
@@ -16,6 +22,8 @@ void P::onUpdate()
 void P::onRender()
 {
     win.draw(s);
+    win.draw(t1);
+    win.draw(t2);
 }
 
 void P::onEvent(sf::Event &ev)
