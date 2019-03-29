2f217193fb5ebb352c4f12dfe841d38940ab7663
root <root@localhost>
2019-03-29 16:32:04 +0100

    p h

--- p.h
+++ p.h
@@ -1,13 +1,15 @@
-#ifndef P_H
+ #ifndef P_H
 #define P_H
 
 #include <SFML/Graphics.hpp>
 #include "Program.h"
 #include "Console.h"
+#include "Touchable.h"
 
 class P : public Program
 {
     sf::CircleShape s;
+    Touchable t1,t2;
 public:
     P();
     virtual void onRender() override;
