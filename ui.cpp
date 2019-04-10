683f8a9a3baaa0fff980663fa9add7f251843059
root <root@localhost>
2019-04-10 10:34:03 +0200

    u cpp

--- ui.cpp
+++ ui.cpp
@@ -1,17 +1,19 @@
 #include "ui.h"
 
-Desctription default_ui[5] =
+constexpr int def_pos_x = 150;
+constexpr int def_pos_y = 500;
+constexpr int w  = 50;
+
+const Description default_ui[5] =
 {
 	 // { pos:{x,y}, vert:{ {x1,y1}, {x2,y2}, ...}  }  
-	{  {,},  { {,},{,},{,} }  },
-	{  {,},  { {,},{,},{,} }  },
-	{  {,},  { {,},{,},{,} }  },
-	{  {,},  { {,},{,},{,} }  },
+	{  {def_pos_x,def_pos_y -60},  { {-w,-w},{w,-w},{0,w} }  },
+	{  {def_pos_x+60, def_pos_y},  { {w,-w},{w,w},{-w,0} }  },
+	{  {def_pos_x, def_pos_y+60},  { {w,w},{-w,w},{0,-w} }  },
+	{  {def_pos_x-60, def_pos_y},  { {-w,w},{-w,-w},{w,0} }  },
 	
-	{  {,},  { {,},{,},{,} }  }
+	{  {900,500},  { {-w,-w},{w,-w},{w,w},{-w,w } }  }
 };
-
-
 
 UI::UI(){}
 
@@ -29,7 +31,18 @@ void UI::addNode( Touchable * t )
 
 void UI::createDefault()
 {
-	auto t = new Touchable(300,300, {{-50,-50},{50,-50},{0,50}});
-	t->setFillColor(sf::Color::Green);
-	addNode(t);
+	for(int i=0; i<5; ++i)
+	{
+		auto t = new Touchable(default_ui[i].pos.x, default_ui[i].pos.y, default_ui[i].vert);
+		Console::get()<<"added "<<i<<"\n";
+			t->setFillColor(sf::Color(255,255,255,128));
+	addNode(t);		
+	
+	}
+}
+
+void UI::setHandler(int t, TouchableHandler & h)
+{
+	if(t >= nodes.size()) return;
+	nodes.at(t)->setHandler(h);
 }
