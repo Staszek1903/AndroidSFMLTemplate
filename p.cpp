40940a96a39c908b41b1d8faf20aab753270ecde
root <root@localhost>
2019-04-10 10:33:24 +0200

    p cpp

--- p.cpp
+++ p.cpp
@@ -1,16 +1,10 @@
 #include "p.h"
 
 P::P()
-:t1(100,500,100,100), t2(300,500, 100,100)
 {
     s.setPosition(sf::Vector2f (200,200));
     s.setFillColor(sf::Color::Blue);
     s.setRadius(100);
- 	t1.setFillColor(sf::Color::Blue);
- 	t2.setFillColor(sf::Color::Green);
- 	Renderer::get().addDrawable(t1);
- 	Renderer::get().addDrawable(t2);
- 	
  	ui.createDefault();
 }
 
