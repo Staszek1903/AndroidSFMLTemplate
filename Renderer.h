f8e4aa0f920d5b6444a6f310cfbd29b32ef64345
root <root@localhost>
2019-04-10 08:23:56 +0200

    renderer h

--- Renderer.h
+++ Renderer.h
@@ -3,17 +3,19 @@
 
 #include <SFML/Graphics.hpp>
 #define PIXEL_WIDTH 1200.0f
+#include <vector>
 
 class Renderer
 {
 	static Renderer * instance;
 	static sf::RenderWindow * win_pointer;
 	
-	sf::RenderWindow & win;
 	sf::View view;
 	float aspect_ratio= 0.0f;
-
-	Renderer(sf::RenderWindow &win);
+	
+	std::vector <sf::Drawable*> drawables;
+	
+	Renderer();
 public:
 	static void setWindow(sf::RenderWindow & win);
 	static Renderer& get();
@@ -22,6 +24,9 @@ public:
 	void mapCtoP(float xi, float yi, float & xo,float & yo);
 	void mapPtoC(float xi, float yi, float & xo,float & yo);
 	
+	
+	void addDrawable(sf::Drawable & d);
+	void drawAll();
 	void draw(sf::Drawable & d);	
 };
 
