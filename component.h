09c7472c110e9206f1c7ba2038a17fcdd99fbca1
root <root@localhost>
2019-04-12 12:25:16 +0200

    component h

--- component.h
+++ component.h
@@ -1,3 +1,26 @@
 #ifndef COMPONENT_H_
 #define COMPONENT_H_
-                            
+
+#include <string>
+
+class BaseComponent
+{
+public:
+	BaseComponent();
+};
+
+template<class C>
+class Component : public BaseComponent
+{
+public:
+	Component(){}
+	
+	static std::string getName()
+	{
+		return typeid(C).name();
+	}
+	
+};
+
+
+ #endif      
