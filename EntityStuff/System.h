99e042ee083a7fdd42da540e228b4fa611d047dd
root <root@localhost>
2019-04-17 10:40:09 +0200

    system h

--- EntityStuff/System.h
+++ EntityStuff/System.h
@@ -1,12 +1,20 @@
 #ifndef SYSTEM_H_
 #define SYSTEM_H_
 
+#include <vector>
+#include "Component.h"
+
 class System
 {
 public:
-    System() = delete;
+    System();
+	~System();
+protected:
 	std::vector<Component *> components;
-	
+	virtual void addComponent(Component * ) = 0;
+
+public:
+
 };
 
 #endif
