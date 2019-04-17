093d95cd3c98f1f43cf13ddb7625cf7f00c093c4
root <root@localhost>
2019-04-17 09:29:50 +0200

    entitystuff h

--- EntityStuff/EntityStuff.h
+++ EntityStuff/EntityStuff.h
@@ -1,12 +1,16 @@
-
 #ifndef ENTITYSTUFF_H_
 #define ENTITYSTUFF_H_
 
-#include "Component.h"
+#include <vector>
 
-class EntityStuff
+#include "../Singleton.hpp"
+#include "Component.h"
+#include "System.h"
+
+class EntityStuff : public Singleton<EntitiStuff>
 {
-	
+	std::vector <Component *> components;
+	std::vector <System *> systems;
 };
 
 #endif
