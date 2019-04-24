a88a9d3a5e093fc649914649b36eeb51b0d62b55
root <root@localhost>
2019-04-24 08:56:10 +0200

    entc

--- EntityStuff/entity_stuff.cpp
+++ EntityStuff/entity_stuff.cpp
@@ -12,11 +12,21 @@ EntityStuff::~EntityStuff()
 
 int EntityStuff::get_component_id(const std::string & name)
 {
-    if(components_ids.find(name) != components_ids.end())
+    if(components_ids.find(name) == components_ids.end())
     {
+    	Console::get()<<"next_id: "<<next_id<<"\n";
         components_ids[name]= next_id;
         next_id <<=1;
     }
 
     return (components_ids[name]);
+}
+
+void EntityStuff::update_systems()
+{
+	Console::get()<<"update systemow\n";
+	for(BaseSystem * sys : systems)
+	{
+		sys->update();
+	}
 }
