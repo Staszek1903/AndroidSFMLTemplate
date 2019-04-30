672548be6ca63923da5593a311cd35f09b661486
root <root@localhost>
2019-04-30 10:41:33 +0200

    ents c

--- EntityStuff/entity_stuff.cpp
+++ EntityStuff/entity_stuff.cpp
@@ -10,18 +10,6 @@ EntityStuff::~EntityStuff()
     }
 }
 
-int EntityStuff::get_component_id(const std::string & name)
-{
-    if(components_ids.find(name) == components_ids.end())
-    {
-    	Console::get()<<"next_id: "<<next_id<<"\n";
-        components_ids[name]= next_id;
-        next_id <<=1;
-    }
-
-    return (components_ids.at(name));
-}
-
 void EntityStuff::update_systems()
 {
 	Console::get()<<"update systemow\n";
@@ -29,11 +17,4 @@ void EntityStuff::update_systems()
 	{
 		sys->update();
 	}
-}
-
-int EntityStuff::get_component_id(BaseComponent & c)
-{
-	std::string name = c.getClassName();
-	if(components_ids.find(name) == components_ids.end()) return 0;
-	return components_ids.at(name);
 }
