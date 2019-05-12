c27f104d696ccd3013c46351fcaa886aea7c10e9
root <root@localhost>
2019-05-12 11:40:25 +0200

    event

--- EntityStuff/entity_stuff.cpp
+++ EntityStuff/entity_stuff.cpp
@@ -1,17 +1,17 @@
 #include "entity_stuff.h"
 
 EntityStuff::EntityStuff()
-    :entity_manager(component_manager), system_manager(entity_manager){}
+    :entity_manager(component_manager), system_manager(entity_manager, event_manager){}
 
 EntityStuff::~EntityStuff()
 {
     system_manager.release();
 }
 
-void EntityStuff::update_systems()
+void EntityStuff::update_systems(double dt)
 {
     Console::get()<<"update systemow\n";
-    system_manager.update();
+    system_manager.update(dt);
 }
 
 ComponentManager &EntityStuff::get_component_menager()
@@ -27,4 +27,9 @@ SystemManager & EntityStuff::get_system_manager()
 EntityManager & EntityStuff::get_entity_manager()
 {
     return entity_manager;
+}
+
+EventManager & EntityStuff::get_event_manager()
+{
+	return event_manager;
 }
