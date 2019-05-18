83931a7a509d45a21f15d8a594f803f71ca3b043
root <root@localhost>
2019-05-18 17:15:23 +0200

    not finished releases

--- EntityStuff/entitymanager.h
+++ EntityStuff/entitymanager.h
@@ -32,13 +32,44 @@ public:
      * @return chosen entities;
      */
     std::vector<size_t> get_entities(size_t mask);
-
+	
+	/**
+		* creates ne entity_entry
+		* @return entity data index
+		*/
     size_t create_entity_data();
+    
+    /**
+    	* return pointer to entity data entry at coresponding index
+    	* @param idex - entity data index
+    	*/
     EntityData * get_entity_data_ptr(size_t index);
-
+	
+	/**
+		* deletes entity data entry and releases all of its components
+		* @param data_index - entity_data index
+		*/
+	void realeaseEntity(size_t data_index);
+	
+	/**
+		* alocates new component bound to entity
+		* @param entity_id 
+		* @return component wraper
+		*/
     template< class C >
     Component<C> addComponent(size_t entity_id);
-
+	
+	/**
+		* releases component
+		* @param entity_data - entity data index
+		*/    
+    template< class C>
+    void releaseComponent(size_t entity_data);
+	
+	/**
+		* @param entity_id
+		* @return Component wraper
+		*/
     template< class C >
     Component<C> getComponent(size_t entity_id);
 };
@@ -50,6 +81,13 @@ Component<C> EntityManager::addComponent(size_t entity_id)
     temp.allocate(c_manager, entity_id, Component<C>::get_id());
     return temp;
 }
+
+template< class C>
+void EntityManager::releaseComponent(size_t entity_id)
+{
+	c_manager.release_component(entity_id, Component<C>::get_id());
+}
+  
 
 template<class C>
 Component<C> EntityManager::getComponent(size_t entity_id)
