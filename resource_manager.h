33f283e2029a57eb7188042278b4192eb292fc4b
root <root@localhost>
2019-05-20 20:29:44 +0200

    resourcemanager

---
+++ resource_manager.h
@@ -0,0 +1,25 @@
+#ifndef RESOURCE_MANAGER_H_
+#define RESOURCE_MANAGER_H_
+
+#include "resource_getter.h"
+#include "singleton.hpp"
+#include <SFML/Graphics.hpp>
+
+class ResourceManager:public Singleton < ResourceManager >
+{
+	ResourceManager(){};
+	~ResourceManager(){};
+
+	friend Singleton < ResourceManager > ;	
+	
+	RESOURCE(sf::Texture, textures);
+
+};
+
+//ResourceManager::ResourceManager()
+//{}
+
+//ResourceManager::~ResourceManager()
+//{}
+
+#endif
