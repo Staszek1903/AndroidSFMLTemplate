e18c45c248df51b19ee776e4dba1669b7be77b37
root <root@localhost>
2019-05-31 11:58:27 +0200

    programstage

--- p.cpp
+++ p.cpp
@@ -2,33 +2,34 @@
 
 
 P::P()
-{
-	
-	ResourceManager::get();
+{ }
+ 
+ P::~P()
+ {}
+ 
+ void P::init()
+ {
+ 	Console::get()<<"P::init\n";
+ 	ResourceManager::get();
 	
     s.setPosition(sf::Vector2f (200,200));
     s.setFillColor(sf::Color::Blue);
     s.setRadius(100);
- 	ui.createDefault();
- }
- 
- P::~P()
- {
- 	
+ 	ui.createDefault();	
  }
 
-void P::onUpdate()
+void P::update(double dt)
 {
     sf::Vector2f pos = s.getPosition();
     s.setPosition({pos.x+1,pos.y+1});
 }
 
-void P::onRender()
+void P::render(sf::RenderWindow & win)
 {
     win.draw(s);
 }
 
-void P::onEvent(sf::Event &ev)
+void P::input(sf::Event &ev)
 {
 	if(ev.type == sf::Event::TouchBegan)
 	{ Script sc;	sc.load_from_file("./textures/textures.conf"); }
@@ -46,3 +47,6 @@ void P::onEvent(sf::Event &ev)
 	*/
 	}
 }
+
+void P::release() 
+{}
